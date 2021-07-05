1e82b9be620a93b322cf2280e9ff913a838e2263
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-05 12:22:51 -0400

    Assignment 4

---
+++ src/main/java/ucf/assignments/ListMakerController.java
@@ -0,0 +1,19 @@
+package ucf.assignments;
+
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Ivedent Lloyd III
+ */
+
+import javafx.event.ActionEvent;
+
+public class ListMakerController {
+    public void createNewListClicked(ActionEvent actionEvent) {
+    }
+
+    public void editListClicked(ActionEvent actionEvent) {
+    }
+
+    public void autoGenerateListClicked(ActionEvent actionEvent) {
+    }
+}
