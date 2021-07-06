10ae57b2b1259d9bab8e784745d08b730abc9365
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-05 20:51:19 -0400

    Assignment 4

--- src/test/java/ucf/assignments/ListMakerEditorTest.java
+++ src/test/java/ucf/assignments/ListMakerEditorTest.java
@@ -9,13 +9,24 @@ class ListMakerEditorTest {
     @Test
     void deleteListClicked() {
         // reads the file
+        // takes in the user input
+        // deletes the selected lists in the file
+        // reads the file making sure the deleted list does not exist
+        // closes file
     }
 
     @Test
     void editListClicked() {
+        // reads the file
+        // deletes string selected by the user
+        // replaces the string with new string
+        // writes to file
+        // reads the file and makes sure new file matches input
+        // closes file
     }
 
     @Test
     void returnToListMaker() {
+        // make sure this button closes current menu and returns to initial window
     }
 }
