0e07e5497822b7b55040a66216f15428c57ac6b5
Ivedent Lloyd III <ivedent.lloyd@gmail.com>
2021-07-05 21:02:59 -0400

    Assignment 4

--- src/main/java/ucf/assignments/NewListMaker.java
+++ src/main/java/ucf/assignments/NewListMaker.java
@@ -6,10 +6,13 @@ package ucf.assignments;
  */
 
 public class NewListMaker {
+    // opens the file
     // prompts the user to input a title for a new list
     // prompts the user for the item in the list
     // prompts for the description of the item
     // prompts the user for the due date of the item
     // allows the user to add another item to the list
     // allows user to break at make a new list or return to the first window
+    // writes the new list and other data to the file
+    // closes file
 }
